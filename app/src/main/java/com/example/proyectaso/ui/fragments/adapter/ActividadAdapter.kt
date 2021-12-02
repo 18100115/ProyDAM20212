1bcb8d61a2c3345621058066839b0c7b40c60041
Cgustavo24 <gustavocoronel123@gmail.com>
2021-12-02 17:30:40 -0500

    Add comment profile

--- app/src/main/java/com/example/proyectaso/ui/fragments/adapter/ActividadAdapter.kt
+++ app/src/main/java/com/example/proyectaso/ui/fragments/adapter/ActividadAdapter.kt
@@ -1,4 +1,13 @@
 package com.example.proyectaso.ui.fragments.adapter
 
-class ActividadAdapter {
-}
+import android.view.View
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+//class ActividadAdapter (private var lstActividad: List<ActividadAdapter>):RecyclerView.Adapter<ActividadAdapter.ViewHolder>(){
+ //   class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+   // {
+
+
+   // }
+//}
