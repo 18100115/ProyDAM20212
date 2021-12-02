5bd39a0772f1480e2df0c5ef4f17d90cad27e841
Luis Rosa <18100115@ue.edu.pe>
2021-12-02 12:59:32 -0500

    Add comment profile

--- app/src/main/java/com/example/proyectaso/ui/fragments/MainActivity.kt
+++ app/src/main/java/com/example/proyectaso/ui/fragments/MainActivity.kt
@@ -17,36 +17,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val tipo = intent.getStringExtra("param")
+        //val tipo = intent.getStringExtra("param")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //Se valida el perfil
-        if (tipo == "Voluntario"){
-            val navView: BottomNavigationView = binding.navView
-            val navController = findNavController(R.id.nav_host_fragment_activity_main)
-            // Passing each menu ID as a set of Ids because each
-            // menu should be considered as top level destinations.
-            val appBarConfiguration = AppBarConfiguration(
-                setOf(
-                    R.id.navactividades, R.id.navtienda, R.id.navhorario, R.id.navperfil
-                )
-            )
-            setupActionBarWithNavController(navController, appBarConfiguration)
-            navView.setupWithNavController(navController)
-        }else if (tipo == "Auspiciador"){
-            val navView2: BottomNavigationView = binding.navView2
-            val navController = findNavController(R.id.nav_host_fragment_activity_main)
-            // Passing each menu ID as a set of Ids because each
-            // menu should be considered as top level destinations.
-            val appBarConfiguration = AppBarConfiguration(
-                setOf(
-                    R.id.navactividades2, R.id.navperfil2
-                )
-            )
-            setupActionBarWithNavController(navController, appBarConfiguration)
-            navView2.setupWithNavController(navController)
-        }
 
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                    R.id.navactividades, R.id.navtienda, R.id.navhorario, R.id.navperfil
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
 
     }
