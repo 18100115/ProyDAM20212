eb914bb0a8f394cc0311fc06e720b9c0159bd958
Cgustavo24 <gustavocoronel123@gmail.com>
2021-12-02 17:18:52 -0500

    Add comment profile

--- app/src/main/java/com/example/proyectaso/ui/fragments/model/Actividad.kt
+++ app/src/main/java/com/example/proyectaso/ui/fragments/model/Actividad.kt
@@ -1,10 +1,10 @@
 package com.example.proyectaso.ui.fragments.model
 
 data class Actividad(
-    val nombre:String,
-    val descripcion:String,
-    val ubicacion:String,
-    val recompensa:String,
-    val num_voluntarios :String,
-    val fecha_hora:String,
+    val nombre:String="",
+    val descripcion:String="",
+    val ubicacion:String="",
+    val recompensa:String="",
+    val num_voluntarios :String="",
+    val fecha_hora:String="",
 )
