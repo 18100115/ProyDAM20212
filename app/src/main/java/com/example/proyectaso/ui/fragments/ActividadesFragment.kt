5096eed09f5b2d0ae0b125aaf6b6d8cdc7b1bab3
Cgustavo24 <gustavocoronel123@gmail.com>
2021-12-02 16:49:17 -0500

    Add comment profile

--- app/src/main/java/com/example/proyectaso/ui/fragments/ActividadesFragment.kt
+++ app/src/main/java/com/example/proyectaso/ui/fragments/ActividadesFragment.kt
@@ -7,7 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import com.example.proyectaso.R
+import com.example.proyectaso.ui.fragments.activities.LoginActivity
+import com.example.proyectaso.ui.fragments.activities.Registro_NA_Activity
 
 class ActividadesFragment : Fragment() {
 
@@ -19,8 +22,9 @@ class ActividadesFragment : Fragment() {
         val view:View = inflater.inflate(R.layout.fragment_actividades, container, false)
         val btnAgregar: Button = view.findViewById(R.id.btn_AgregarAct)
 
-        btnAgregar.setOnClickListener {
-
+        btnAgregar.setOnClickListener{
+            val intent = Intent(activity, Registro_NA_Activity::class.java)
+            startActivity(intent)
         }
         return  view
 
---
+++ app/src/main/java/com/example/proyectaso/ui/fragments/activities/Registro_NA_Activity.kt
@@ -0,0 +1,71 @@
+package com.example.proyectaso.ui.fragments.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import com.example.proyectaso.R
+import com.example.proyectaso.ui.fragments.ActividadesFragment
+import com.example.proyectaso.ui.fragments.MainActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
+
+class Registro_NA_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_na)
+
+        val txtNombreEvent: EditText = findViewById(R.id.edt_nombreE)
+        val txtDescrip : EditText = findViewById(R.id.edt_desc)
+        val txtUbic: EditText = findViewById(R.id.edt_ubic)
+        val txtRecom: EditText = findViewById(R.id.edt_recom)
+        val txtNvol: EditText = findViewById(R.id.edt_nvol)
+        val txtFecha: EditText = findViewById(R.id.edt_fecha)
+        val btnRegistroNA: Button = findViewById(R.id.btn_registroNA)
+        val db = FirebaseFirestore.getInstance()
+
+
+        btnRegistroNA.setOnClickListener{
+            var nombre = txtNombreEvent.text.toString()
+            var descripcion = txtDescrip.text.toString()
+            var ubicacion = txtUbic.text.toString()
+            var recompensa = txtRecom.text.toString()
+            var num_voluntarios = txtNvol.text.toString()
+            var fecha_hora = txtFecha.text.toString()
+
+            val nuevoActividad = Actividad(nombre,descripcion,ubicacion,recompensa,num_voluntarios,fecha_hora)
+            val id:UUID = UUID.randomUUID()
+
+            db.collection("actividad")
+                .document(id.toString())
+                .set(nuevoActividad)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Registro Completado", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, ActividadesFragment::class.java)
+                    startActivity(intent)
+                }.addOnFailureListener {
+                    Toast.makeText(this, "Ocurrió un problema", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, ActividadesFragment::class.java)
+                    startActivity(intent)
+                }
+
+
+        }
+
+
+    }
+}
+
+private fun enviarMensaje(vista: View ,mensaje:String){
+    Snackbar.make(vista,mensaje,Snackbar.LENGTH_LONG).show()
+}
+data class Actividad(
+    val nombre:String,
+    val descripcion:String,
+    val ubicacion:String,
+    val recompensa:String,
+    val num_voluntarios :String,
+    val fecha_hora:String,
+)
