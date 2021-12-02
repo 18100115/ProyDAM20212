91603d1a8d70343b0c592d4f745d37e5eeb006e6
Cgustavo24 <gustavocoronel123@gmail.com>
2021-12-02 17:35:20 -0500

    Add comment profile

--- app/src/main/java/com/example/proyectaso/ui/fragments/adapter/ActividadAdapter.kt
+++ app/src/main/java/com/example/proyectaso/ui/fragments/adapter/ActividadAdapter.kt
@@ -1,13 +1,28 @@
 package com.example.proyectaso.ui.fragments.adapter
 
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.proyectaso.R
 
-//class ActividadAdapter (private var lstActividad: List<ActividadAdapter>):RecyclerView.Adapter<ActividadAdapter.ViewHolder>(){
- //   class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
-   // {
+class ActividadAdapter (private var lstActividad: List<ActividadAdapter>):RecyclerView.Adapter<ActividadAdapter.ViewHolder>(){
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView)
+    {
+       // val tvNombre: TextView = itemView.findViewById(R.id.tvNombre)
 
+    }
 
-   // }
-//}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
